6f14870244381499ae40ec74d53743d421c8606e
Eric <ericscottlagergren@gmail.com>
2014-05-01 18:00:01 -0700

    reorganized python script so it made sense

--- autowget.py
+++ autowget.py
@@ -9,56 +9,65 @@ import sh
 import os
 import sys
 import datetime
-sys.stdout = open('/var/log/autowget.log', 'w')
+sys.stdout = open('/var/log/autowget.log', 'a')
+sys.stderr = open('/var/log/autowget.log', 'a')
 
 ######################
 ### FILL VARIABLES ###
 ######################
 
-url = 'http://www.example.org'
-file_name = '/path/to/the/file'
-file_path = '/path/to/the/' # Path to file's containing folder
-repo = '/path/to/repo'
+urls = ['http://www.example.org', 'https://google.com', 'https://microsoft.com']
+file_path = '/path/to/directory/file/is/in'
+repo = '/path/to/github/pages/repository' # If you only want to commit one portion of your repository, then make sure you put that path here
 commitMessage = 'new mirror' # Your commit message 
 addAll = '--all' # Adds all tracked files; can change to just one file
 branch = 'master' # Branch you're committing/pushing to
 
-##############################
-### Uncomment if using FTP ###
-##############################
+############################################################
+### FTP OPTIONS - MAKE SURE TO CHANGE THESE IF USING FTP ###
+############################################################
 
-#ftp = FTP('ftp.server.com')
-#username = "" # FTP server username
-#password = "" # FTP server password
-#server_path = "" # Path to folder where you want to save file(s)
-
-# If login is anonymous (why it would be is beyond me) then go to line 55 "ftp.login(username, password)" and delete the two words "username," and "password" so it looks like this: ftp.login()
+#useFtp = True
+useFtp = False
+# If login is anonymous (why it would be is beyond me) then leave the password and username variables alone
+username = "" # FTP server username
+password = "" # FTP server password
+server_path = "" # Path to folder where you want to save file(s)
+server = "" # Server name... e.g. ftp.debian.org
 
 ####################################
 ### SHOULDN'T NEED TO TOUCH THIS ###
 ####################################
 
-def getUrl():
-	response = urllib.request.urlopen(url)
-	with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
-	   	shutil.copyfileobj(response, out_file)
-getUrl()
+def getUrl(urls):
+	for url in urls:
+		new_file_path = file_path + url.rsplit('.com', 1)[0].replace('https://', '').replace('http://', '').replace('.', '').replace('www', '').replace('/', '') + ".html"
+		response = urllib.request.urlopen(url)
+		#if (os.path.isfile(new_file_path) == False): # Don't mess with this... there for potential future use
+		with open(new_file_path, 'wb') as out_file:
+			shutil.copyfileobj(response, out_file)
+#		'''elif (os.path.isfile(new_file_path) == True): # Same with these lines
+#			for i, urls in enumerate(urls, start=0):	 # And this
+#				number_name = new_file_path.replace('.html', '') + "-%s.html" % i # This too
+#				with open(number_name, 'wb') as out_file:	# This one as well
+#					shutil.copyfileobj(response, out_file)''' # And finally this one
+getUrl(urls)
 
 def gitPush():
 	git = sh.git.bake(_cwd=repo)
 	print(git.status())
-	#print(git.checkout(branch))
 	print(git.add(addAll))
 	print(git.commit(m=commitMessage))
 	print(git.push('origin', branch))
 	print(git.status())
 
 
-if 'ftp' in locals():
+if useFtp == True:
+	ftp = FTP(server) # FTP server... e.g. ftp.debian.org
 	ftp.login(username, password)
 	ftp.cwd(server_path)
 	os.chdir(file_path)
-	ftp.storlines("STOR " + file_name, open(file_name, 'r'))
+	ftp.storlines("STOR " + file_path, open(file_path, 'r'))
 else: 
 	gitPush()
 
